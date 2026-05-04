7f3b6a48ef7b125022f3a68a6a80428b9c82f843
Omarcito <balderascabralomar@gamil.com>
2026-05-03 22:21:54 -0600

    Funciones temporales para crear admin y developer2.3

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,16 +5,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { Customer } from '../customers/entities/customer.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+      ConfigModule,
       TypeOrmModule.forFeature([User, Customer]),
-      JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h'},
-    }),],
+      JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject:[ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        global: true,
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, ConfigService],
 })
 export class AuthModule {}
